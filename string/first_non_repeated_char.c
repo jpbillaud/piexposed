dbcd0bb9b4433fc1597acae98e96f662b8a582ff
Jean-Pascal Billaud <jp.billaud@gmail.com>
2011-03-19 09:16:27 -0700

    Initial repository for string

---
+++ string/first_non_repeated_char.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+
+/*
+ * Write an efficient function to find the first nonrepeated character
+ * in a string. For instance the first nonrepeated character in "total"
+ * is 'o' and the first nonrepeated in "teeter" is 'r'.
+ */
