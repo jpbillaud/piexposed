daf163c73e4a4ea2bd32de31bfcff66c49d35c19
Jean-Pascal Billaud <jp.billaud@gmail.com>
2011-03-18 18:57:32 -0700

    Initial math repository

---
+++ math/loopy_loops.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/ruby
+
+=begin
+Print the numbers from 1 to 1000 without using any loop or conditional
+statements. Don’t just write the printf() or cout statement 1000 times.
+=end
---
+++ math/sloane.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/ruby
+# -*- coding: utf-8 -*-
+
+=begin
+The "Look and Say" sequence, Sloane number A005150, begins 1, 11, 21, 1211,
+111221, 312211, 13112221, 1113213211, ... Each term is constructed from its
+predecessor by stating the frequency and number of each group of like digits.
+For instance, the term after 1211 is ´one 1, one 2, and two 1s¡, or 111221.
+=end
