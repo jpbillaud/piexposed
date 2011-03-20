fa6599d19937f631721495fe8131cd6c549dc92d
Jean-Pascal Billaud <jp.billaud@gmail.com>
2011-03-20 15:08:19 -0700

    Implementation of function to find the first non repeated character of string

--- string/first_non_repeated_char.c
+++ string/first_non_repeated_char.c
@@ -5,3 +5,29 @@
  * in a string. For instance the first nonrepeated character in "total"
  * is 'o' and the first nonrepeated in "teeter" is 'r'.
  */
+
+char
+first_non_repeated_char(char *str)
+{
+   char count_arr[128] = {0, };
+   char *tmp = str;
+
+   /*
+    * First fill in the counter table
+    */
+   while (*tmp) {      
+      count_arr[*(tmp++)]++;
+   }
+
+   /*
+    * Then look for the first non-repeated character
+    */
+   while (*str) {      
+      if (count_arr[*str] == 1) {
+         return *str;
+      }
+      str++;
+   }   
+
+   return '\0';
+}
