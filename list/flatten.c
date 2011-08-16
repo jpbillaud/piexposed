4e19e28ce194cd2b7ddef66fcd7c7a0af15aa2aa
Jean-Pascal Billaud <billaud@vmware.com>
2011-08-16 11:23:00 -0700

    Add some more comments for flatten.c and a general question for marbles brain teaser

--- list/flatten.c
+++ list/flatten.c
@@ -6,6 +6,39 @@
  * Flatten a tree-linked list so that all nodes appear in a single
  * level, doubly-linked list. You are given the head and the tail
  * of the first level of the list.
+ *
+ *    --head                  --tail
+ *   |                       |
+ *  ---     ---     ---     --- 
+ * | 5 |   | 6 |   | 3 |   | 4 |
+ * |   |-->|   |-->|   |-->| x |
+ * |   |<--|   |<--|   |<--| x |
+ * |   |   | x |   | x |   | x |
+ *  ---     ---     ---     ---
+ *   |
+ *   |
+ *  ---     --- 
+ * | 2 |   | 1 |
+ * |   |-->| x |
+ * |   |<--| x |
+ * |   |   |   |
+ *  ---     --- 
+ *   |       |
+ *   |       |
+ *  ---     ---     ---     --- 
+ * | 7 |   | 8 |   | 9 |   | 2 |
+ * | x |   |   |-->|   |-->| x |
+ * | x |   |   |<--|   |<--| x |
+ * | x |   | x |   | x |   |   |
+ *  ---     ---     ---     ---
+ *                           |
+ *                           |
+ *                          ---     --- 
+ *                         | 4 |   | 0 |
+ *                         |   |-->| x |
+ *                         |   |<--| x |
+ *                         | x |   | x |
+ *                          ---     --- 
  */
 
 void
