9353698a520fda07be6ca980e2eec6a311637818
Jean-Pascal Billaud <jp.billaud@gmail.com>
2011-09-14 00:16:49 -0700

    Reverse a singly-linked list exercise

---
+++ list/reverse.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+#include "list.h"
+
+/*
+ * Reverse a given a singly-linked list.
+ */
+
+struct s_list_node *
+slist_reverse(struct s_list_node *head)
+{
+   struct s_list_node *prev, *ptr, *next;
+
+   for (prev = NULL, ptr = head; ptr; prev = ptr, ptr = next) {
+      next = ptr->next;
+
+      ptr->next = prev;
+   }
+
+   return prev;
+}
