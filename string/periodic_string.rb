9a488eea5e39163f4335d12ec3d23415310b111b
Jean-Pascal Billaud <jp.billaud@gmail.com>
2011-11-20 16:26:56 -0800

    Detect string periodicity using rotation

--- string/periodic_string.rb
+++ string/periodic_string.rb
@@ -3,25 +3,19 @@ Strings such as "000000", "101010" and "11011101" are said periodic since they a
 repetitions of a given pattern.
 =end
 
-def periodic_string? word
+def periodic_string? string
+  #
+  # The easiest way to deal with this is to compare the string with a shifted
+  # version of itself. When the shifted version equals to the original string,
+  # then the string is periodic with a period equals to the number of 
+  # shifts + 1.
+  #
 
-  #
-  # The easiest way to deal with this is to iteratively increase the pattern,
-  # one character at a time. Note that some patterns do no make sense to try
-  # so we just skip them.
-  #
+  string_to_a = string.chars.to_a
 
-  #
-  # No point to test a pattern size bigger than half of the given word.
-  #
-  (0..word.size/2).each do |index|
-
-    #
-    # If the pattern size does not fit perfeclty we just skip it.
-    #
-    next if word.size.modulo(index+1) != 0
-    nb_patterns = word.size / (index+1)
-    return true if word.slice(0..index)*nb_patterns == word
+  (0..string.size/2).inject(string.chars.to_a) do |shift_string, period|
+    return [true, period + 1] if shift_string.rotate! == string_to_a
+    shift_string
   end
-  false
+  return [false, -1]
 end
