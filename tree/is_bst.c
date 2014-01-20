5428c889b18d0e7a6b5f904b7c018f6a97b7a7a7
Jean-Pascal (_jpb_) Billaud <jp.billaud@gmail.com>
2014-01-20 09:19:18 +0000

    simpler version of is_bst

--- tree/is_bst.c
+++ tree/is_bst.c
@@ -4,45 +4,30 @@
  * Given a binary tree, find out whether or not it is a binary search tree.
  */
 
-#define MIN(a,b) ((a<b) ? a : b)
-#define MAX(a,b) ((a>b) ? a : b)
-
 static int
-btree_is_bst_rec(struct b_tree_node_t *root, int *pmin, int *pmax)
+btree_is_bst_rec(struct b_tree_node_t *root,
+                 unsigned int min,
+                 unsigned int max)
 {
-   unsigned int lmin = -1, rmin = -1;
-   int lmax = 0, rmax = 0;
+    if (!root) {
+        return 1;
+    }
 
-   if (root->left) {
-      if (!btree_is_bst_rec(root->left, &lmin, &lmax)) {
-         return 0;
-      }
+    if (root->value < min || root->value > max) {
+        return 0;
+    }
 
-      if (root->value < lmax) {
-         return 0;
-      }
-   }
+    if (!btree_is_bst_rec(root->left, min, root->value)) {
+        return 0;
+    }
 
-   if (root->right) {
-      if (!btree_is_bst_rec(root->right, &rmin, &rmax)) {
-         return 0;
-      }
-
-      if (root->value > rmin) {
-         return 0;
-      }
-   }
-
-   *pmin = MIN(lmin, root->value);
-   *pmax = MAX(rmax, root->value);
-   return 1;
+    return btree_is_bst_rec(root->right, root->value, max);
 }
 
 int
 btree_is_bst(struct b_tree_node_t *root)
 {
-   int min, max;
-   return btree_is_bst_rec(root, &min, &max);
+   return btree_is_bst_rec(root, 0, -1);
 }
 
 int main()
@@ -51,7 +36,7 @@ int main()
   struct b_tree_node_t *non_bst_root = DF_NODE(10, DF_NODE(6, DF_LEAF(7), DF_LEAF(3)), DF_NODE(12, DF_LEAF(11), DF_LEAF(15)));
   struct b_tree_node_t *non_bst_root_2 = DF_NODE(10, DF_NODE(6, DF_LEAF(3), DF_LEAF(7)), DF_NODE(12, DF_LEAF(9), DF_LEAF(15)));
 
-  printf("is bst %u\n", btree_is_bst(bst_root));
-  printf("is bst %u\n", btree_is_bst(non_bst_root));
-  printf("is bst %u\n", btree_is_bst(non_bst_root_2));
+  printf("is bst %u should be 1\n", btree_is_bst(bst_root));
+  printf("is bst %u should be 0\n", btree_is_bst(non_bst_root));
+  printf("is bst %u should be 0\n", btree_is_bst(non_bst_root_2));
 }
